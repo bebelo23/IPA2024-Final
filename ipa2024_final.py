bc2f4218a004d0621c7ef7cc8e27509fe87c6eed
bebelo23 <118843029+bebelo23@users.noreply.github.com>
2024-10-25 15:07:58 +0700

    finish part1

--- ipa2024_final.py
+++ ipa2024_final.py
@@ -1,24 +1,32 @@
 #######################################################################################
-# Yourname:
-# Your student ID:
-# Your GitHub Repo: 
+# Yourname: Panalee Chuckseeda
+# Your student ID: 65070127
+# Your GitHub Repo: https://github.com/bebelo23/IPA2024-Final.git
 
 #######################################################################################
 # 1. Import libraries for API requests, JSON formatting, time, os, (restconf_final or netconf_final), netmiko_final, and ansible_final.
 
-<!!!REPLACEME with code for libraries>
+import requests
+import json
+import time
+import restconf_final
+import os
+from dotenv import load_dotenv
+
+# โหลด Environment Variable จากไฟล์ .env
+load_dotenv(dotenv_path='venv/.env')
 
 #######################################################################################
 # 2. Assign the Webex access token to the variable ACCESS_TOKEN using environment variables.
 
-ACCESS_TOKEN = os.environ."<!!!REPLACEME with os.environ method and environment variable!!!>"
+ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
 
 #######################################################################################
 # 3. Prepare parameters get the latest message for messages API.
 
 # Defines a variable that will hold the roomId
 roomIdToGetMessages = (
-    "<!!!REPLACEME with roomID of the IPA2024 Webex Teams room!!!>"
+    "Y2lzY29zcGFyazovL3VybjpURUFNOnVzLXdlc3QtMl9yL1JPT00vZDYwNzgxYTAtNWU5Yy0xMWVmLWI0MTUtZWQ5NGJjZjg4YjM4"
 )
 
 while True:
@@ -31,22 +39,24 @@ while True:
     getParameters = {"roomId": roomIdToGetMessages, "max": 1}
 
     # the Webex Teams HTTP header, including the Authoriztion
-    getHTTPHeader = {"Authorization": <!!!REPLACEME!!!>}
+    getHTTPHeader = {"Authorization": ACCESS_TOKEN}
 
-# 4. Provide the URL to the Webex Teams messages API, and extract location from the received message.
-    
+    # 4. Provide the URL to the Webex Teams messages API, and extract location from the received message.
+
     # Send a GET request to the Webex Teams messages API.
     # - Use the GetParameters to get only the latest message.
     # - Store the message in the "r" variable.
     r = requests.get(
-        "<!!!REPLACEME with URL of Webex Teams Messages API!!!>",
-        params=<!!!REPLACEME with HTTP parameters!!!>,
-        headers=<!!!REPLACEME with HTTP headers!!!>,
+        "https://webexapis.com/v1/messages",
+        params=getParameters,
+        headers=getHTTPHeader,
     )
     # verify if the retuned HTTP status code is 200/OK
     if not r.status_code == 200:
         raise Exception(
-            "Incorrect reply from Webex Teams API. Status code: {}".format(r.status_code)
+            "Incorrect reply from Webex Teams API. Status code: {}".format(
+                r.status_code
+            )
         )
 
     # get the JSON formatted returned data
@@ -58,39 +68,35 @@ while True:
 
     # store the array of messages
     messages = json_data["items"]
-    
+
     # store the text of the first message in the array
     message = messages[0]["text"]
     print("Received message: " + message)
 
     # check if the text of the message starts with the magic character "/" followed by your studentID and a space and followed by a command name
     #  e.g.  "/66070123 create"
-    if message.startswith("<!!!REPLACEME!!!>"):
+    if message.startswith("/65070127"):
 
         # extract the command
-        command = <!!!REPLACEME!!!>
+        command = message[len("/65070127 ") :].strip()
         print(command)
 
-# 5. Complete the logic for each command
+        # 5. Complete the logic for each command
 
         if command == "create":
-            <!!!REPLACEME with code for create command!!!>     
+            responseMessage = restconf_final.create()
         elif command == "delete":
-            <!!!REPLACEME with code for delete command!!!>
+            responseMessage = restconf_final.delete()
         elif command == "enable":
-            <!!!REPLACEME with code for enable command!!!>
+            responseMessage = restconf_final.enable()
         elif command == "disable":
-            <!!!REPLACEME with code for disable command!!!>
+            responseMessage = restconf_final.disable()
         elif command == "status":
-            <!!!REPLACEME with code for status command!!!>
-         elif command == "gigabit_status":
-            <!!!REPLACEME with code for gigabit_status command!!!>
-        elif command == "showrun":
-            <!!!REPLACEME with code for showrun command!!!>
+            responseMessage = restconf_final.status()
         else:
             responseMessage = "Error: No command or unknown command"
-        
-# 6. Complete the code to post the message to the Webex Teams room.
+
+        # 6. Complete the code to post the message to the Webex Teams room.
 
         # The Webex Teams POST JSON data for command showrun
         # - "roomId" is is ID of the selected room
@@ -98,41 +104,49 @@ while True:
         # - "files": is a tuple of filename, fileobject, and filetype.
 
         # the Webex Teams HTTP headers, including the Authoriztion and Content-Type
-        
+
         # Prepare postData and HTTPHeaders for command showrun
-        # Need to attach file if responseMessage is 'ok'; 
+        # Need to attach file if responseMessage is 'ok';
         # Read Send a Message with Attachments Local File Attachments
         # https://developer.webex.com/docs/basics for more detail
 
-        if command == "showrun" and responseMessage == 'ok':
-            filename = "<!!!REPLACEME with show run filename and path!!!>"
-            fileobject = <!!!REPLACEME with open file!!!>
-            filetype = "<!!!REPLACEME with Content-type of the file!!!>"
+        if command == "showrun" and responseMessage == "ok":
+            filename = "show_running_config.txt"
+            fileobject = (
+                "C:/Users/Admin/Desktop/finalll/IPA2024-Final/show_running_config.txt"
+            )
+            filetype = "text/plain"
             postData = {
-                "roomId": <!!!REPLACEME!!!>,
+                "roomId": roomIdToGetMessages,
                 "text": "show running config",
-                "files": (<!!!REPLACEME!!!>, <!!!REPLACEME!!!>, <!!!REPLACEME!!!>),
+                "files": (filename, fileobject, filetype),
             }
-            postData = MultipartEncoder(<!!!REPLACEME!!!>)
+            postData = MultipartEncoder(postData)
             HTTPHeaders = {
-            "Authorization": ACCESS_TOKEN,
-            "Content-Type": <!!!REPLACEME with postData Content-Type!!!>,
+                "Authorization": ACCESS_TOKEN,
+                "Content-Type": "application/json",
             }
+
         # other commands only send text, or no attached file.
         else:
-            postData = {"roomId": <!!!REPLACEME!!!>, "text": <!!!REPLACEME!!!>}
+            postData = {"roomId": roomIdToGetMessages, "text": responseMessage}
             postData = json.dumps(postData)
 
             # the Webex Teams HTTP headers, including the Authoriztion and Content-Type
-            HTTPHeaders = {"Authorization": <!!!REPLACEME!!!>, "Content-Type": <!!!REPLACEME!!!>}   
+            HTTPHeaders = {
+                "Authorization": ACCESS_TOKEN,
+                "Content-Type": "application/json",
+            }
 
         # Post the call to the Webex Teams message API.
         r = requests.post(
-            "<!!!REPLACEME with URL of Webex Teams Messages API!!!>",
-            data=<!!!REPLACEME!!!>,
-            headers=<!!!REPLACEME!!!>,
+            "https://webexapis.com/v1/messages",
+            data=postData,
+            headers=HTTPHeaders,
         )
         if not r.status_code == 200:
             raise Exception(
-                "Incorrect reply from Webex Teams API. Status code: {}".format(r.status_code)
+                "Incorrect reply from Webex Teams API. Status code: {}".format(
+                    r.status_code
+                )
             )
